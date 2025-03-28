f90b441b9b33837eab139b0ea088aadf0ff3e21b
moncy george <moncyg@gmail.com>
2025-03-28 09:26:38 -0500

    Update4

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 # Predefined responses for the chatbot
 responses = {
-    "hi": "Hi there! How can I help you?",
-    "hello": "Hi there! How can I help you?",
-    "how are you": "I'm doing great, thanks for asking! How about you?",
-    "bye": "Goodbye! Have a nice day!",
+    "hi": "Hi there! Thanks for visiting Sharon Dallas site, how can I help you?",
+    "hello": "Hi there! Thanks for visiting Sharon Dallas site, how can I help you?",
+    "Service times": "Our Sunday worship service starts at 10 am",
+    "Address": "940 Barnes Bridge Rd, Mesquite, TX 75150",
+    "bye": "Have a nice day! God Bless you",
 }
 
 @app.route('/')
@@ -17,7 +18,7 @@ def index():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.form['user_message'].lower()  # Get the message from the user
-    response_message = responses.get(user_message, "Sorry, I don't understand that.")  # Get response from predefined responses
+    response_message = responses.get(user_message, "Sorry, I might not be able to answer that.")  # Get response from predefined responses
     return jsonify({'response': response_message})  # Return the response as JSON
 
 if __name__ == '__main__':
